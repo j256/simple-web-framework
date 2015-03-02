3b67bad396f4a097654eda7b618a78419f856e80
Gray Watson <gray.github@mailnull.com>
2015-03-02 12:42:17 -0500

    Added better freemarker example.

--- src/test/java/com/j256/simplewebframework/example/FreemarkerExample.java
+++ src/test/java/com/j256/simplewebframework/example/FreemarkerExample.java
@@ -1,5 +1,9 @@
 package com.j256.simplewebframework.example;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.jws.WebMethod;
 import javax.jws.WebService;
 import javax.ws.rs.GET;
@@ -8,10 +12,20 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerCollection;
 import org.eclipse.jetty.server.nio.SelectChannelConnector;
 
+import com.j256.simplewebframework.displayer.FileResultDisplayer;
+import com.j256.simplewebframework.displayer.ResultDisplayer;
 import com.j256.simplewebframework.displayer.StringResultDisplayer;
+import com.j256.simplewebframework.freemarker.FreemarkerHtmlDisplayer;
+import com.j256.simplewebframework.handler.LoggingHandler;
 import com.j256.simplewebframework.handler.ServiceHandler;
+import com.j256.simplewebframework.resource.FileLocator;
+import com.j256.simplewebframework.resource.LocalResourceHandler;
+
+import freemarker.template.Configuration;
+import freemarker.template.Version;
 
 /**
  * Little sample program which starts a test web-server up on port 8080 which demonstrates some of the features of the
@@ -19,7 +33,7 @@ import com.j256.simplewebframework.handler.ServiceHandler;
  * 
  * @author graywatson
  */
-public class SimpleExample {
+public class FreemarkerExample {
 
 	/** default web port that we will server jetty results on */
 	private static final int DEFAULT_WEB_PORT = 8080;
@@ -35,15 +49,50 @@ public class SimpleExample {
 		connector.setReuseAddress(true);
 		server.addConnector(connector);
 
+		// display freemarker files
+		FreemarkerHtmlDisplayer htmlDisplayer = new FreemarkerHtmlDisplayer();
+		// find templates in this directory
+		File templateDir = new File("src/test/resources/com/j256/simplewebframework/example");
+		FileLocator fileLocator = new FileLocator(templateDir, new String[] { "index.html" });
+		htmlDisplayer.setFileLocator(fileLocator);
+		// freemarker configuration on where to find templates
+		Configuration freeMarkerConfig = new Configuration(new Version("2.3.22"));
+		freeMarkerConfig.setDirectoryForTemplateLoading(templateDir);
+		htmlDisplayer.setTemplateConfig(freeMarkerConfig);
+
+		/*
+		 * setup the handlers which handle the request
+		 */
+
 		// create a service handler
 		ServiceHandler serviceHandler = new ServiceHandler();
 		// register our service
 		serviceHandler.registerWebService(new OurService());
 		// register a displayer of String results
 		serviceHandler.registerResultDisplayer(new StringResultDisplayer());
+		serviceHandler.registerResultDisplayer(htmlDisplayer);
+
+		// handles CSS, JS, images and other files served raw
+		LocalResourceHandler resourceHandler = new LocalResourceHandler();
+		// process the html files using the freemarker displayer
+		Map<String, ResultDisplayer> displayerMap = new HashMap<String, ResultDisplayer>();
+		displayerMap.put("html", htmlDisplayer);
+		resourceHandler.setFileExtensionDisplayers(displayerMap);
+		// everything else uses the file result displayer
+		resourceHandler.setDefaultDisplayer(new FileResultDisplayer());
+		resourceHandler.setFileLocator(fileLocator);
+
+		// a collection of handlers which are tried one after another
+		HandlerCollection handlerCollection = new HandlerCollection();
+		handlerCollection.addHandler(serviceHandler);
+		handlerCollection.addHandler(resourceHandler);
+
+		// wrap the handler collection in a logging handler so we can see the page-view logs
+		LoggingHandler loggingHandler = new LoggingHandler();
+		loggingHandler.setHandler(handlerCollection);
 
 		// this could be a collection of handlers or ...
-		server.setHandler(serviceHandler);
+		server.setHandler(loggingHandler);
 		server.start();
 
 		System.out.println("Web-server running on port " + DEFAULT_WEB_PORT);
@@ -55,8 +104,7 @@ public class SimpleExample {
 	}
 
 	/**
-	 * Small web service which presents a simple form to the user and displays the results if any. This couple be in its
-	 * own class but is here just to make the example be small.
+	 * Small web service which presents a simple form to the user and displays the results if any.
 	 */
 	@WebService
 	@Produces({ "text/html" })
